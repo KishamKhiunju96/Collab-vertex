cf10b522587ef3b15f035985f796b701d257ce0d
kisham <kishamkhiunju96@gmail.com>
2025-12-30 16:14:05 +0545

    uff

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
 
-
-export default function LoginForm(){
+export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [remember, setRemember] = useState(false);
@@ -11,59 +11,84 @@ export default function LoginForm(){
     e.preventDefault();
     console.log({ email, password, remember });
   };
+
   return (
-    <div className="min-h-screen justify-center flex items-center bg-background-light">
-      <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8 space-y-6">
-        <h1 className="text-4xl text-center font-bold text-text-primary  mt-10">Login to Collab-Vertex</h1>
-        <form onSubmit = {handleSubmit} className="space-y-5">
-          <div>
-            <label className="block text-sm font-medium mb-1 text-text-primary">
-              Email
-            </label>
-            <input
-              type="email"
-              required
-              value={email}
-              onChange={(e) => setEmail (e.target.value)}
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border border-green-400 rounded-lg ">
-            </input>
-          </div>
-          <div>
-            <label className="block text-sm font-medium mb-1 text-text-primary">
-              Password
-            </label>
-            <input
-              type="password"
-              required
-              value={password}
-              onChange={(e) => setPassword (e.target.value)}
-              placeholder="********"
-              className="w-full px-4 py-2 border border-green-400 rounded-lg">
-            </input>
-          </div>
-          <div className="flex items-center justify-between">
-            <label className="inline-flex items-center">
+    <div className="min-h-screen flex items-center justify-center bg-background-light px-4">
+      <div className="w-full max-w-6xl h-[600px] bg-white rounded-2xl shadow-xl overflow-hidden grid grid-cols-1 md:grid-cols-2">
+        <div className="p-12 flex flex-col justify-center ">
+          <h1 className="text-4xl text-center font-bold text-text-primary mb-8">
+            Login to Collab-Vertex
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-5">
+            <div>
+              <label className="block text-sm font-medium mb-1 text-text-primary">
+                Email
+              </label>
               <input
-                type="checkbox"
-                checked = {remember}
-                onChange={(e) => setRemember (e.target.checked)}
-                className="h-4 w-4 text-green-400 rounded ">
-                </input>
-              <span className="ml-2 text-sm text-text-primary">Remember me</span>
-            </label>
-            <a href="" className="text-sm text-red-400 ">Forget Password</a>
-          </div>
-          <button
-           type="submit"
-           className="w-full py-2 px-4 bg-green-400 hover:bg-green-300 text-text-primary font-semibold rounded-lg transition-transform transform hover:scale-105">
-             Login
-          </button>
-          <p className="text-center text-sm text-text-primary">
-            Don't have an account?{""}
-          <a href="/Register" className="text-red-400 px-4 fontfont-medium hover:text-red-600 transition">Register</a>
-          </p>
-        </form>
+                type="email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 border border-green-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium mb-1 text-text-primary">
+                Password
+              </label>
+              <input
+                type="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="********"
+                className="w-full px-4 py-2 border border-green-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-300"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <label className="inline-flex items-center">
+                <input
+                  type="checkbox"
+                  checked={remember}
+                  onChange={(e) => setRemember(e.target.checked)}
+                  className="h-4 w-4 text-green-400 rounded"
+                />
+                <span className="mt-2 text-sm text-text-primary">
+                  Remember me
+                </span>
+              </label>
+              <a href="#" className="text-sm text-red-400 hover:underline">
+                Forgot Password
+              </a>
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-green-400 hover:bg-green-300 text-text-primary font-semibold rounded-lg transition-transform transform hover:scale-105">
+              Login
+            </button>
+            <p className="text-center text-sm text-text-primary">
+              Don&apos;t have an account?
+              <a
+                href="/Register"
+                className="text-red-400 px-2 font-medium hover:text-red-600 transition"
+              >
+                Register
+              </a>
+            </p>
+          </form>
+        </div>
+        <div className="hidden md:block relative h-full w-full bg-green-50">
+          <Image
+            src="/images/collaboration.jpg"
+            alt="Login "
+            width={400}
+            height={400}
+            className="absolute inset-0 w-full h-full object-cover scale-105"
+          />
+        </div>
+        <div className="absolute inset-0 bg-black/40"></div>
+
       </div>
     </div>
   );
